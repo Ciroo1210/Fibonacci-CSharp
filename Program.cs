1845f91074e119c434907a7905d1a02263a6f354
Ciro <cdante2022@gmail.com>
2024-08-17 21:35:51 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int num1 = 0;
+            int num2 = 1;
+            int a;
+            Console.WriteLine(0);
+            for (int i = 0; i < 10; i++)
+            {
+                a = num1;
+                num1 = num1 + num2;
+                num2 = a;
+                
+                Console.WriteLine(num1);
+                
+            }
+
+        }
+
+    }
+}
